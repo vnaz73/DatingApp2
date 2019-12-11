3f76ebf6759b0c6bf0148e7f0eba5d2686c3c8de
v_n_z list.ru <v_n_z@list.ru>
2019-12-11 03:36:18 +0300

    Initial Commit

---
+++ datingapp.api/Controllers/ValueController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using datingapp.api.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+
+namespace datingapp.api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ValueController : ControllerBase
+    {
+        public DataContext _context { get; set; }
+        
+        private readonly ILogger<ValueController> _logger;
+
+        public ValueController(ILogger<ValueController> logger, DataContext context)
+        {
+            _logger = logger;
+            this._context = context;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetValue()
+        {
+            var values = await _context.Values.ToListAsync();
+            return Ok(values);
+        }
+
+         [HttpGet("{id}")]
+        public async Task<IActionResult> GetValue(int id)
+        {
+            var value =await  _context.Values.FirstOrDefaultAsync(x => x.Id == id);
+            return Ok(value);
+        }
+    }
+}
---
+++ datingapp.api/Data/DataContext.cs
@@ -0,0 +1,15 @@
+using datingapp.api.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace datingapp.api.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Value> Values {get; set;}
+    }
+}
